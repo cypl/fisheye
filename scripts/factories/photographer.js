75cb148bba3471dae9f91a4ea3e58884cd67a9cb
cypl <cyrille.plenchette@me.com>
2022-10-31 14:33:18 +0100

    début du projet - index

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -1,16 +1,63 @@
-function photographerFactory(data) {
-    const { name, portrait } = data;
+function photographerFactory(data) {  // data représente un élément “photographer” de l'objet “photographers”
 
+    // je ne comprends pas vraiment ça {} ?
+    const { name, id, portrait, city, country, tagline, price } = data;  // les éléments entre {} représentent les types de datas de l'élément “photographer” 
+    //
+
+    // cette variable représente le lien vers l'image du photographe, 
+    // ${portrait} est le nom du fichier qui est dans l'élément “photographer”
     const picture = `assets/photographers/${portrait}`;
 
+    // cette fonction crée les éléments de la carte de chacun des photographes
     function getUserCardDOM() {
+        
+        // on crée une balise <article> dans laquelle on va créer les autres éléments 
         const article = document.createElement( 'article' );
-        const img = document.createElement( 'img' );
-        img.setAttribute("src", picture)
-        const h2 = document.createElement( 'h2' );
-        h2.textContent = name;
-        article.appendChild(img);
-        article.appendChild(h2);
+        article.classList.add("photographer_card");
+
+        // on crée une balise <img> pour le portrait 
+        const imgPhotographerFigure = document.createElement( 'figure' );
+        const imgPhotographer = document.createElement( 'img' );
+        imgPhotographerFigure.appendChild(imgPhotographer);
+        imgPhotographer.setAttribute("alt", name);
+        imgPhotographer.setAttribute("src", picture);
+        imgPhotographer.classList.add("photographer_card__img");
+        imgPhotographerFigure.classList.add("photographer_card__figure");
+        
+        // on crée une balise <h2> pour le nom du photographe 
+        const photographerName = document.createElement( 'h2' );
+        photographerName.textContent = name;
+        photographerName.classList.add("photographer_card__name");
+
+        // on crée une balise <a> dans laquelle on va placer le portrait + le nom du photographe
+        const photographerLink = document.createElement( 'a' );
+        photographerLink.classList.add("photographer_card__link");
+        const photographerLinkTarget = `photographer.html`; //${ID}  => utiliser URL Pattern ? https://developer.mozilla.org/en-US/docs/Web/API/URL_Pattern_API
+        photographerLink.setAttribute("href", photographerLinkTarget);
+        photographerLink.appendChild(imgPhotographerFigure);
+        photographerLink.appendChild(photographerName);
+
+        // on crée une balise <p> dans laquelle on va placer la localisation
+        const photographerLocalisation = document.createElement( 'p' );
+        photographerLocalisation.classList.add("photographer_card__localisation");
+        photographerLocalisation.textContent = city + ", " + country;
+
+        // on crée une balise <p> dans laquelle on va placer la philosophie
+        const photographerPhilosophy = document.createElement( 'p' );
+        photographerPhilosophy.classList.add("photographer_card__philosophy");
+        photographerPhilosophy.textContent = tagline;
+
+        // on crée une balise <p> dans laquelle on va placer le tarif
+        const photographerPrice = document.createElement( 'p' );
+        photographerPrice.classList.add("photographer_card__price");
+        photographerPrice.textContent = price + "€/jour";
+
+        // on intègre les différentes éléments de article dans article
+        article.appendChild(photographerLink);
+        article.appendChild(photographerLocalisation);
+        article.appendChild(photographerPhilosophy);
+        article.appendChild(photographerPrice);
+
         return (article);
     }
     return { name, picture, getUserCardDOM }
--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -1,46 +1,124 @@
-    async function getPhotographers() {
-        // Penser à remplacer par les données récupérées dans le json
-        const photographers = [
-            {
-                "name": "Ma data test",
-                "id": 1,
-                "city": "Paris",
-                "country": "France",
-                "tagline": "Ceci est ma data test",
-                "price": 400,
-                "portrait": "account.png"
-            },
-            {
-                "name": "Autre data test",
-                "id": 2,
-                "city": "Londres",
-                "country": "UK",
-                "tagline": "Ceci est ma data test 2",
-                "price": 500,
-                "portrait": "account.png"
-            },
-        ]
-        // et bien retourner le tableau photographers seulement une fois
-        return ({
-            photographers: [...photographers, ...photographers, ...photographers]})
+// async function getJsonPhotographers(){
+//     fetch("../data/photographers.json")
+//         .then(function(res) {
+//             if(res.ok){
+//                 return res.json();
+//             }
+//         })
+//         .then(function(value) {
+//             //console.log(value.photographers);   // => retourne une promise sous la forme d'un Array
+//             return value.photographers;
+//         })
+//         .catch(function(error){
+//             //erreurs
+//         });
+// }
+
+
+async function getPhotographers() {
+        
+
+    // fetch("../data/photographers.json")
+    //     .then(function(res) {
+    //         if(res.ok){
+    //             return res.json();
+    //         }
+    //     })
+    //     .then(function(value) {
+    //         //console.log(value.photographers);   // => retourne une promise sous la forme d'un Array
+    //         return value.photographers;
+    //     })
+    //     .catch(function(error){
+    //         //erreurs
+    //     });
+
+    // comment transformer le résultat de la promise en donnée exploitable ??
+
+    // return ({
+    //          photographers: [...photographers]})
+        
+
+    // tableau à remplacer par le résulat de la Promise ci-dessus
+    const photographers = [
+        {
+            "name": "Mimi Keel",
+            "id": 243,
+            "city": "London",
+            "country": "UK",
+            "tagline": "Voir le beau dans le quotidien",
+            "price": 400,
+            "portrait": "MimiKeel.jpg"
+        },
+        {
+            "name": "Ellie-Rose Wilkens",
+            "id": 930,
+            "city": "Paris",
+            "country": "France",
+            "tagline": "Capturer des compositions complexes",
+            "price": 250,
+            "portrait": "EllieRoseWilkens.jpg"
+        },
+        {
+            "name": "Tracy Galindo",
+            "id": 82,
+            "city": "Montreal",
+            "country": "Canada",
+            "tagline": "Photographe freelance",
+            "price": 500,
+            "portrait": "TracyGalindo.jpg"
+        },
+        {
+            "name": "Nabeel Bradford",
+            "id": 527,
+            "city": "Mexico City",
+            "country": "Mexico",
+            "tagline": "Toujours aller de l'avant",
+            "price": 350,
+            "portrait": "NabeelBradford.jpg"
+        },
+        {
+            "name": "Rhode Dubois",
+            "id": 925,
+            "city": "Barcelona",
+            "country": "Spain",
+            "tagline": "Je crée des souvenirs",
+            "price": 275,
+            "portrait": "RhodeDubois.jpg"
+        },
+        {
+            "name": "Marcel Nikolic",
+            "id": 195,
+            "city": "Berlin",
+            "country": "Germany",
+            "tagline": "Toujours à la recherche de LA photo",
+            "price": 300,
+            "portrait": "MarcelNikolic.jpg"
+        }
+    ];
+
+    return ({
+        photographers: [...photographers]})
+              
     }
 
-    async function displayData(photographers) {
-        const photographersSection = document.querySelector(".photographer_section");
 
-        photographers.forEach((photographer) => {
-            //les data de chaque photographes sont chargées selon le template de la fonction photographerFactory();
-            const photographerModel = photographerFactory(photographer);
-            const userCardDOM = photographerModel.getUserCardDOM();
-            photographersSection.appendChild(userCardDOM);
-        });
-    };
 
-    async function init() {
-        // Récupère les datas des photographes
-        const { photographers } = await getPhotographers();
-        displayData(photographers);
-    };
-    
-    init();
+async function displayData(photographers) {
+    const photographersSection = document.querySelector(".photographer_section");
+
+    photographers.forEach((photographer) => {
+        //les data de chaque photographes sont chargées selon le template de la fonction photographerFactory();
+        const photographerModel = photographerFactory(photographer); // (photographer) est un élément de l'objet photographers
+        const userCardDOM = photographerModel.getUserCardDOM();
+        photographersSection.appendChild(userCardDOM);
+    });
+};
+
+async function init() {
+    // Récupère les datas des photographes
+    const { photographers } = await getPhotographers();  // Pourquoi y-a-t'il des accolades autour de “photographers“ ? 
+    displayData(photographers);
+};
+
+init();
     
