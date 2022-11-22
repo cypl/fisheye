f55e5828398458b8a7ed717e1781277fe4d0322e
cypl <cyrille.plenchette@me.com>
2022-11-22 09:20:23 +0100

    refactorisation api

--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -4,26 +4,10 @@ const urlParameters = new URLSearchParams(queryString);
 const idPhotographer = parseInt(urlParameters.get('id')) // = ID du photographe
 
 
-// // On fait une requête sur le fichier JSON, et on retourne une promise 
-// async function photographersApi() {
-//     const urlData = "../data/photographers.json";
-//     const photographers = await fetch(urlData); 
-//     return photographers.json(); // = Promise
-// }
-
-
-// // On récupère le resultat de la promise issue de photographersApi(), et on retourne le resultat une fois que la requête a abouti 
-// async function getPhotographers() {
-//     return await photographersApi(); // = Datas qui résultent de la Promise
-// };
-
-
 // Dans le tableau, on cherche le photographe qui a cet ID, et on retourne l'objet correspondant
 async function findPhotographer(photographers) {
     // on filtre le tableau des photographes pour retouver le bon, grâce à son ID.
     const singlePhotographer = photographers.find(photographer => photographer.id === idPhotographer);
-    //console.log(singlePhotographer);
-    //return await singlePhotographer;
     return singlePhotographer;
 };
 
