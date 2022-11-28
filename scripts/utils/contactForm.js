60afa8a3ae663d060934c10c523d5328fa753535
cypl <cyrille.plenchette@me.com>
2022-11-28 16:09:04 +0100

    update CSS

--- scripts/utils/contactForm.js
+++ scripts/utils/contactForm.js
@@ -6,6 +6,7 @@ function displayModal() {
 	body.style.overflow = "hidden";
 }
 
+
 // Fonction pour fermer la fenêtre modale
 function closeModal() {
     const modal = document.getElementById("contact_modal");
@@ -14,11 +15,13 @@ function closeModal() {
 	body.style.overflow = "auto";
 }
 
+
 // Fermer la fenêtre modale avec le bouton
 const contactPhotographerClose = document.getElementById("contact-photographer-close");
 contactPhotographerClose.addEventListener('click', (event) => {
     closeModal();
   });
+
 
 // Fermer la fenêtre modale avec la touche Escap
 document.addEventListener("keydown", (e) => {
@@ -27,7 +30,8 @@ document.addEventListener("keydown", (e) => {
     }
 });
 
-async function photographerForm(data) {
+
+function photographerForm(data) {
     const { name } = data;  // les éléments entre {} représentent les types de datas de l'élément “photographer” 
     const formTitle = document.getElementById("form_title");
     const formTitleName = document.createElement("span");
@@ -35,4 +39,22 @@ async function photographerForm(data) {
     formTitle.appendChild(formTitleName);
 }
 
+
+function printFormDatas(){
+    const fieldFirstName = document.getElementById("form__firstname");
+    const fieldLastName = document.getElementById("form__lastname");
+    const fieldEmail = document.getElementById("form__email");
+    const fieldMessage = document.getElementById("form__message");
+    console.log(`Nouveau message de : 
+${fieldFirstName.value} ${fieldLastName.value}
+Email : ${fieldEmail.value}
+Message : ${fieldMessage.value}`);
+}
+// Validation du formulaire
+const modal = document.getElementById("contact_modal");
+const formSubmit = modal.querySelector(".contact_button");
+formSubmit.addEventListener('click', (event) => {
+    printFormDatas();
+    event.preventDefault();
+});
 
