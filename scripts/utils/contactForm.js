99ecfe1172b4aeddf253f742d1c73098a1602baa
cypl <cyrille.plenchette@me.com>
2023-01-26 16:45:51 +0100

    ajout de commentaires

--- scripts/utils/contactForm.js
+++ scripts/utils/contactForm.js
@@ -5,7 +5,10 @@ const main = document.getElementById("main");
 const formSubmit = modal.querySelector(".contact_button");
 let modalOpen = false;
 
-// Fonction pour ouvrir la fenêtre modale
+
+/**
+ * Fonction pour ouvrir la fenêtre modale.
+ */
 function displayModal() {
     modal.style.display = "flex";
 	body.style.overflowY = "hidden";
@@ -15,7 +18,10 @@ function displayModal() {
     modalOpen = true;
 }
 
-// Fonction pour fermer la fenêtre modale
+
+/**
+ * Fonction pour fermer la fenêtre modale.
+ */
 function closeModal() {
     const buttonOpenModal = document.getElementById("contact-photographer");
     modal.style.display = "none";
@@ -27,12 +33,18 @@ function closeModal() {
     modalOpen = false;
 }
 
-// Fermer la fenêtre modale avec le bouton
+
+/**
+ * Évènement pour fermer la fenêtre modale avec la croix.
+ */
 contactClose.addEventListener('click', (event) => {
     closeModal();
 });
 
-// Fermer la fenêtre modale avec la touche Escap
+
+/**
+ * Évènement pour fermer la fenêtre modale avec la touche Escape.
+ */
 document.addEventListener("keydown", (e) => {
     if(e.key === "Escape") {
         if(modalOpen){
@@ -41,7 +53,11 @@ document.addEventListener("keydown", (e) => {
     }
 });
 
-// Une fonction pour récupérer le nom du photographe et l'affiche dans le header du formulaire
+
+/**
+ * Fonction pour récupérer le nom du photographe et l'afficher dans le header du formulaire.
+ * @param {Object} data correspond à l'objet photographer
+ */
 function photographerForm(data) {
     const { name } = data;  // les éléments entre {} représentent les types de datas de l'élément “photographer” 
     const formTitle = document.getElementById("form_title");
@@ -50,7 +66,10 @@ function photographerForm(data) {
     formTitle.appendChild(formTitleName);
 }
 
-// Une fonction pour afficher les données du formulaire dans la console
+
+/**
+ * Fonction pour afficher les données du formulaire dans la console.
+ */
 function printFormDatas(){
     const fieldFirstName = document.getElementById("form__firstname");
     const fieldLastName = document.getElementById("form__lastname");
@@ -62,16 +81,23 @@ Email : ${fieldEmail.value}
 Message : ${fieldMessage.value}`);
 }
 
-// Validation du formulaire
+
+/**
+ * Évènement pour valider le formulaire lorsque l'utilisateur clique sur le bouton submit
+ */
 formSubmit.addEventListener('click', (event) => {
     validateForm();
     event.preventDefault();
 });
 
 
-
-
-// Fonction qui est utilisée pour les différentes fonctions de validation des champs
+/**
+ * Fonction qui est utilisée pour différentes fonctions de validation des champs du formulaire.
+ * @param {*} condition correspond à la condition selon laquelle la validation est soumise
+ * @param {Element} formData correspond à l'élément HTML qui enveloppe le champ
+ * @param {String} ErrorMessage correspond au message qui décrit l'erreur
+ * @returns 
+ */
 function validationDry(condition, formData, ErrorMessage){
     if(condition){ // la saisie est valide
       formData.removeAttribute("data-error");
@@ -82,16 +108,31 @@ function validationDry(condition, formData, ErrorMessage){
     }
     return condition;
   }
-//Validation de la longueur de la saisie
+
+
+/**
+ * Fonction pour valider la longueur de la saisie.
+ * @param {Element} field correspond à l'élément HTML du champ
+ * @param {Element} formData correspond à l'élément HTML qui enveloppe le champ
+ * @returns {Boolean}
+ */
 function validateLength(field,formData){
     return validationDry(field.value.length >= 2, formData, "Vous devez saisir au moins 2 caractères.");
-  }
-//Validation de la saisie de l'email
+}
+
+
+/**
+ * Fonction pour valider l'email.
+ * @param {*} field correspond à l'élément HTML du champ
+ * @param {*} formData correspond à l'élément HTML qui enveloppe le champ
+ * @returns {Boolean}
+ */
 function validateEmail(field,formData){
     let emailString = field.value;
     let emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/; // Regex pour vérifier le contenu de l'email saisi
     return validationDry(emailRegex.test(emailString), formData, "Votre email n'est pas valide.");
 }
+
 
 // Tests au remplissage du formulaire
 const modalContent = document.querySelector(".modal");
@@ -109,28 +150,50 @@ const formDataEmail = document.getElementById("formData__email");
 const messageWrapper = document.getElementById("form__message");
 const formDataMessage = document.getElementById("formData__message");
 
+
+/**
+ * Évènement pour valider le prénom.
+ */
 firstNameWrapper.addEventListener('input', (event) => {
     validateLength(firstNameWrapper,formDataFirstName);
 });
+
+
+/**
+ * Évènement pour valider le nom.
+ */
 lastNameWrapper.addEventListener('input', (event) => {
     validateLength(lastNameWrapper,formDataLastName);
 });
+
+
+/**
+ * Évènement pour valider l'email
+ */
 emailWrapper.addEventListener('input', (event) => {
     validateEmail(emailWrapper,formDataEmail);
 });
+
+
+/**
+ * Évènement pour valider le message
+ */
 messageWrapper.addEventListener('input', (event) => {
     validateLength(messageWrapper,formDataMessage);
 });
 
+
 // Les contenu de chaque champ sera validé, chaque validation est stockée dans un array
 const validatorForm = [false, false, false, false];
-// Fonction pour valider les champs de formulaire
+/**
+ * Fonction pour valider les champs de formulaire.
+ */
 function validateForm(){
     validatorForm[0] = validateLength(firstNameWrapper,formDataFirstName);
     validatorForm[1] = validateLength(lastNameWrapper,formDataLastName);
     validatorForm[2] = validateEmail(emailWrapper,formDataEmail);
     validatorForm[3] = validateLength(messageWrapper,formDataMessage);
-    if(validatorForm.includes(false)){ // Il y a au moins une validation qui est fausse
+    if(validatorForm.includes(false)){ // Il y a au moins une validation qui a échoué
         event.preventDefault();
     }else{ // Tous les champs sont validés 
         event.preventDefault();
@@ -140,7 +203,10 @@ function validateForm(){
     }
 }
 
-// Fonction qui affiche un message de remerciement
+
+/**
+ * Fonction qui affiche un message de remerciement.
+ */
 function thankYouForm(){
     contactForm.style.display = "none";
     const thankYou = document.createElement("p");
@@ -149,7 +215,10 @@ function thankYouForm(){
     modalContent.appendChild(thankYou);
 }
 
-// Fonction pour vider les champs de formulaire
+
+/**
+ * Fonction pour vider les champs de formulaire (une fois que l'on ferme la modale).
+ */
 function emptyForm(){
     firstNameWrapper.value = "";
     formDataFirstName.removeAttribute("data-error");
